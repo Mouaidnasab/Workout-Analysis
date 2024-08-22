9f1c17d317e05bb8ffab04052998982a610e0cf1
Mouaidnasab <65204173+Mouaidnasab@users.noreply.github.com>
2024-08-22 15:13:48 +0300

    change

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 import unittest
@@ -14,7 +15,15 @@ import os
 class PoseDetectionUITests(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        self.driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),
+            options=chrome_options
+        )
         self.driver.get("http://localhost:5500") 
     def test_title(self):
         driver = self.driver
